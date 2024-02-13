467cba2015427bc178cef82d4bd612909a1c4d65
shivakrishna <shivakrishnabandari59@gmail.com>
2024-02-13 17:37:49 +0530

    first commit

---
+++ weather/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Weather from './components/current';
+
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <Weather />
+      </div>
+      
+    </>
+  );
+}
+
+export default App;
---
+++ weather/src/components/apiKeys.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+    key: "cad3a83931bca13b9ecdb428308cb571",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
